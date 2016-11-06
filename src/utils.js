63ae49c978490e5ba153b0f556789da43634331b
pomber <pombopombopombo@gmail.com>
2016-11-06 19:14:58 -0300

    Calculate last week

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,42 @@
 (function(window) {
   'use strict';  
-  var self = window.Utils = window.Utils || {   
+  var self = window.Utils = window.Utils || {  
+
+    /**
+     * Get last seven days including today.
+     * Assumes that log array is sorted asc.
+     */
+    getLastWeek: function(now, log) {
+      var today = new Date(now);
+      var lastSeven = log.slice(-7);
+      var days = [];
+      for (var i = -6; i <= 0; i++) {        
+        var day = new Date(today);
+        day.setDate(today.getDate() + i);
+        days.push(day);
+      }
+
+      return days.map(function(day) {
+        var logTime = lastSeven.find(function(logTime) {
+          var itemDate = new Date(logTime);
+          return itemDate.toDateString() == day.toDateString();
+        });
+
+        if (!logTime) {
+          return {
+            daystring: self.getShortDateString(day)
+          };
+        }
+
+        return self.getDayInfo(logTime);
+      });
+    },
 
     getDayInfo: function(ts) {
       if (!ts) {
         return;
       }
-      
+
       var date = new Date(ts);
       return {
         hourstring: self.getHourString(date),
