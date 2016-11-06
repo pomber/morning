15d2717ae15e06d810addf2a2f4cd36831ab393d
pomber <pombopombopombo@gmail.com>
2016-11-06 18:41:22 -0300

    Refactor utils

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,32 @@
 (function(window) {
   'use strict';  
-  window.Utils = window.Utils || {
+  var self = window.Utils = window.Utils || {   
 
     getDayInfo: function(ts) {
+      if (!ts) {
+        return;
+      }
+      
       var date = new Date(ts);
-      var hourstring = date.toTimeString().split(" ")[0].slice(0, 5);
-      var daystring = date.toLocaleDateString('en-US', {weekday:'short'});
-      var minutes = date.getHours() * 60 + date.getMinutes();
       return {
-        hourstring: hourstring,
-        daystring: daystring,
-        minutes: minutes,
+        hourstring: self.getHourString(date),
+        daystring: self.getShortDateString(date),
+        minutes: self.getMinutesCount(date),
         timestamp: ts,
       };
-    }
+    },
+
+    getHourString: function(date) {
+      return date.toTimeString().split(' ')[0].slice(0, 5);
+    },
+
+    getShortDateString: function(date) {
+      return date.toLocaleDateString('en-US', {weekday:'short'});
+    },
+
+    getMinutesCount: function(date) {
+      return date.getHours() * 60 + date.getMinutes();
+    },
 
   };
 })(window);
